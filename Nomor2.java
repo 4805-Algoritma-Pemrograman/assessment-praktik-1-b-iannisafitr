eb4a9d27cebb73354b5fcc6cb5eecb1b4af93e87
iannisafitr <annisafitria9900@gmail.com>
2024-10-24 10:34:18 +0700

    Update Nomor2.java

--- Nomor2.java
+++ Nomor2.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Nomor2{
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in)
         double totalPendapatan = 0;
         int jumlahTransaksi = 0;
         String inputLagi;
