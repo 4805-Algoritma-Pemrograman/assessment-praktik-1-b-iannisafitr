994ab8f9f88c89b5102de52735e0f1a1e91b64ac
iannisafitr <annisafitria9900@gmail.com>
2024-10-24 10:22:17 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,7 +1,26 @@
-
+import java.util.scanner;
 
 public class Nomor1{
     public static void main(String[] args){
-      
+      Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan total penjualan salesman: ");
+        
+        double totalPenjualan = scanner.nextDouble();
+        double komisi;
+        
+        if (totalPenjualan == 500000) {
+            komisi = totalPenjualan * 0.10;
+        } else {
+            double komisiPertama = 500000 * 0.10;
+            double komisiKedua = (totalPenjualan - 500000) * 0.15;
+            komisi = komisiPertama + komisiKedua;
+        }
+        
+        System.out.println("Komisi yang didapatkan = " + komisi);
+        
+        scanner.close();
+
+    }
+}
     }
 }
